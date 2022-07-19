36a229d26bb90d216fb0186ef736d8ddf6ad3bc8
tbal10 <tbal10@naver.com>
2022-07-19 14:32:12 +0900

    2022/07/19 Book 예제실습

---
+++ Book/Ch05/280.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Ch05
+{
+    internal class _280
+    {
+        class Program
+        {
+            class Test
+            {
+                public void TestMethod()
+                {
+                    Program.Main(new string[] { "" });
+                }
+            }
+
+            public void TestMethod()
+            {
+                Program.Main(new string[] { "" });
+            }
+
+            static void Main(string[] args)
+            {
+
+            }
+        }
+    }
+}
