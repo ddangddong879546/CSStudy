94e83e2ca7307baba95f33b589f99974a15cfaa6
tbal10 <tbal10@naver.com>
2022-07-21 13:17:29 +0900

    2022/07/21 Book 예제실습

---
+++ Book/Ch07/351.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Ch07
+{
+    internal class _351
+    { 
+        class Program
+        {
+            class Parent
+            {
+                public Parent()
+                {
+                    Console.WriteLine("부모 생성자");
+                }
+            }
+
+            class Child : Parent
+            {
+                public Child()
+                {
+                    Console.WriteLine("자식 생성자");
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Child child = new Child();
+            }
+        }
+    }
+}
