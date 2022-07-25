90bc7fb32bdf8e81dd251d589df7e34468c8551f
tbal10 <tbal10@naver.com>
2022-07-25 11:47:14 +0900

    2022/07/25 Ch07 예제실습

---
+++ Ch07/3_List.cs
@@ -0,0 +1,124 @@
+﻿using Ch07.sub1;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 날짜: 2022 / 07 / 25
+ * 이름 : 이세연
+ * 내용 : 컬렉션 리스트 실습하기 p211
+ *
+ * 리스트(List)\
+ * - 배열과 유사한 선형 자료 구조로 배열과 다르게 동적으로 데이터를 처리
+ * - ArrayList 일반화 시켜 더 나은 성능을 가진 List를 사용
+ * 
+ * 
+ */
+namespace Ch07
+{
+    internal class _3_List
+    {
+        static void Main1(string[] args)
+        {
+            ///////////////////////
+            // ArrayList
+            //////////////////////
+            ///
+
+            // ArrayList 생성
+            ArrayList arrList1 = new ArrayList();
+            
+            // 데이터 추가
+            arrList1.Add(1);
+            arrList1.Add(2);
+            arrList1.Add(3);
+            arrList1.Add(4);
+            arrList1.Add(5);
+
+            // 데이터 삽입
+            arrList1.Insert(1, 6);
+
+            // 데이터 삭제
+            arrList1.Remove(6); //6이라는 데이터 삭제
+            arrList1.RemoveAt(0); // 첫번째 칸 삭제
+
+            // 데이터 출력
+            foreach (int n in arrList1)
+            {
+                Console.Write( n + " ");
+            }
+            Console.WriteLine();
+
+            // 다양한 타입의 데이터를 갖는 ArrayList
+            ArrayList arrList2 = new ArrayList();
+            arrList2.Add(1);
+            arrList2.Add(1.23);
+            arrList2.Add(true);
+            arrList2.Add('A');
+            arrList2.Add("Apple");
+
+            for (int i = 0; i < arrList2.Count; i++)
+            {
+                Console.Write(arrList2[i] + " ");
+            }
+            Console.WriteLine();
+
+            ///////////////////////
+            // List
+            //////////////////////
+            
+
+            // List 생성
+            List<int> list1 = new List<int>();
+
+            // 데이터 추가
+            list1.Add(1);
+            list1.Add(2);
+            list1.Add(3);
+            list1.Add(4);
+            list1.Add(5);
+
+            // 데이터 삽입
+            list1.Insert(1, 6);
+
+            // 데이터 삭제
+            list1.Remove(4);
+            list1.RemoveAt(1); //두번째 칸 삭제
+
+            // 데이터 출력
+            foreach (int n in list1)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+
+            // 문자열 리스트
+            List<string> list2 = new List<string>();
+            list2.Add("서울");
+            list2.Add("대전");
+            list2.Add("대구");
+            list2.Add("부산");
+            list2.Add("광주");
+
+            foreach(string city in list2)
+            {
+                Console.Write(city + ' ');
+            }
+            Console.WriteLine();
+
+            // 객체 리스트
+            List<Apple> list3 = new List<Apple>();
+
+            list3.Add(new Apple("한국", 3000));
+            list3.Add(new Apple("미국", 2000));
+            list3.Add(new Apple("일본", 1000));
+
+            Apple apple = list3[0];
+            apple.Show();
+
+            list3[1].Show();
+            list3[2].Show();
+        }
+    }
+}
---
+++ Ch07/4_Dictionary.cs
@@ -0,0 +1,131 @@
+﻿using Ch07.sub1;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 날짜: 2022 / 07 / 25
+ * 이름 : 이세연
+ * 내용 : 컬렉션 딕셔너리 실습하기
+ *
+ * 딕셔너리 (Dictionary)
+ * - 키값(Key-Value) 쌍으로 이루어진 자료구조
+ * - Dictionary는 Hashtable을 일반화 시켜 더 나은 성능을 제공
+ * - List와 더불어 가장 많이 사용하는 자료구조
+ */
+namespace Ch07
+{
+    internal class _4_Dictionary
+    {
+        static void Main1(string[] args)
+        {
+            ///////////////////////////
+            // Hashtable
+            //////////////////////////
+            
+            // 테이블 생성
+            Hashtable table = new Hashtable();
+
+            // 데이터 추가
+            table['A'] = "Apple";
+            table.Add('B', "Banana");
+            table.Add('C', "Cherry");
+
+            // 데이터 삭제
+            table.Remove('C');
+
+            // 데이터 출력
+            foreach (char k in table.Keys)
+            {
+
+                Console.WriteLine("table : " + table[k]);
+            }
+
+            ///////////////////////////
+            // Dictionary
+            //////////////////////////
+
+            // 딕셔너리 생성
+            Dictionary<char, string> dic = new Dictionary<char, string>();
+
+            // 데이터 추가
+            dic['A'] = "Apple";
+            dic.Add('B', "Banana");
+            dic.Add('C', "Cherry");
+
+            // 데이터 삭제
+            dic.Remove('B');
+
+            // 데이터 출력
+            foreach (string fruit in dic.Values)
+            {
+
+                Console.WriteLine("dic : " + fruit);
+            }
+
+            // 딕셔너리 연습 l
+            Dictionary<int, string> people = new Dictionary<int, string>();
+
+            people.Add(101, "김유신");
+            people.Add(102, "김춘추");
+            people.Add(103, "장보고");
+            people.Add(104, "강감찬");
+            people.Add(105, "이순신");
+            
+            foreach(int k in people.Keys)
+            {
+                Console.WriteLine($"k : {k}, V : {people[k]}");
+            }
+
+
+
+            // 딕셔너리 연습 ll
+
+            Dictionary<int, Apple> d1 = new Dictionary<int, Apple>();
+            Dictionary<int, Apple> d2 = new Dictionary<int, Apple>();
+            Dictionary<int, Apple> d3 = new Dictionary<int, Apple>();
+
+            d1.Add(101, new Apple("한국", 3000));
+            d1.Add(102, new Apple("미국", 2000));
+            d1.Add(103, new Apple("일본", 1000));
+
+            d2.Add(201, new Apple("중국", 3000));
+            d2.Add(202, new Apple("대만", 2000));
+            d2.Add(203, new Apple("홍콩", 1000));
+            
+            d3.Add(301, new Apple("호주", 3000));
+            d3.Add(302, new Apple("영국", 2000));
+            d3.Add(303, new Apple("인도", 1000));
+
+
+            List<Dictionary<int, Apple>> apples = new List<Dictionary<int, Apple>>();
+
+            apples.Add(d1);
+            apples.Add(d2);
+            apples.Add(d3);
+
+            // 한국 사과 출력
+            Dictionary <int, Apple> dicApple= apples[0];
+            Apple apple = dicApple[101];
+            apple.Show();
+
+            // 일본 사과 출력
+            apples[0][103].Show();
+
+            // 미국 사과 출력
+            apples[0][102].Show();
+
+            // 대만 사과 출력
+            apples[1][202].Show();
+
+            // 홍콩 사과 출력
+            apples[1][203].Show();
+
+            // 인도 사과 출력
+            apples[2][303].Show();
+
+
+        }
+    }
+}
