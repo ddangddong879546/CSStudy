3f6bdff06a3527f5cd0eae7996b7f57c661c8b7a
tbal10 <tbal10@naver.com>
2022-08-01 13:07:44 +0900

    2022/08/01 Book 예제실습

---
+++ Book/Ch11/511.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Ch11
+{
+    internal class _511
+    {
+        static void Main1(string[] args)
+        {
+            Thread threadA = new Thread(TestMethod);
+            Thread threadB = new Thread(delegate ()
+            {
+
+            });
+            Thread threadC = new Thread(() =>
+            {
+
+            });
+        }
+
+        public static void TestMethod()
+        {
+
+        }
+    }
+}
