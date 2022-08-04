67c0a3cbdf1b7b518eecf884f07d1bbff7ea430b
tbal10 <tbal10@naver.com>
2022-08-04 15:54:36 +0900

    2022/08/04 CarRacing 실습

---
+++ CarRacing/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace CarRacing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox_Car = new System.Windows.Forms.PictureBox();
+            this.pictureBox_redcar = new System.Windows.Forms.PictureBox();
+            this.label_gameOver = new System.Windows.Forms.Label();
+            this.pictureBox_money1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_money3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_money2 = new System.Windows.Forms.PictureBox();
+            this.label_coins = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Car)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_redcar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_money1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_money3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_money2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(182, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 98);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(182, 119);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 98);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(182, 244);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 98);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Location = new System.Drawing.Point(182, 370);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 98);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox_Car
+            // 
+            this.pictureBox_Car.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Car.Image")));
+            this.pictureBox_Car.Location = new System.Drawing.Point(38, 293);
+            this.pictureBox_Car.Name = "pictureBox_Car";
+            this.pictureBox_Car.Size = new System.Drawing.Size(93, 116);
+            this.pictureBox_Car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Car.TabIndex = 4;
+            this.pictureBox_Car.TabStop = false;
+            // 
+            // pictureBox_redcar
+            // 
+            this.pictureBox_redcar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_redcar.Image")));
+            this.pictureBox_redcar.Location = new System.Drawing.Point(15, 40);
+            this.pictureBox_redcar.Name = "pictureBox_redcar";
+            this.pictureBox_redcar.Size = new System.Drawing.Size(65, 99);
+            this.pictureBox_redcar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_redcar.TabIndex = 5;
+            this.pictureBox_redcar.TabStop = false;
+            // 
+            // label_gameOver
+            // 
+            this.label_gameOver.AutoSize = true;
+            this.label_gameOver.BackColor = System.Drawing.Color.Transparent;
+            this.label_gameOver.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_gameOver.ForeColor = System.Drawing.Color.Red;
+            this.label_gameOver.Location = new System.Drawing.Point(12, 177);
+            this.label_gameOver.Name = "label_gameOver";
+            this.label_gameOver.Size = new System.Drawing.Size(367, 64);
+            this.label_gameOver.TabIndex = 7;
+            this.label_gameOver.Text = "Game Over";
+            this.label_gameOver.Visible = false;
+            // 
+            // pictureBox_money1
+            // 
+            this.pictureBox_money1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_money1.Image")));
+            this.pictureBox_money1.Location = new System.Drawing.Point(255, 88);
+            this.pictureBox_money1.Name = "pictureBox_money1";
+            this.pictureBox_money1.Size = new System.Drawing.Size(45, 51);
+            this.pictureBox_money1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_money1.TabIndex = 8;
+            this.pictureBox_money1.TabStop = false;
+            // 
+            // pictureBox_money3
+            // 
+            this.pictureBox_money3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_money3.Image")));
+            this.pictureBox_money3.Location = new System.Drawing.Point(173, 291);
+            this.pictureBox_money3.Name = "pictureBox_money3";
+            this.pictureBox_money3.Size = new System.Drawing.Size(45, 51);
+            this.pictureBox_money3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_money3.TabIndex = 9;
+            this.pictureBox_money3.TabStop = false;
+            // 
+            // pictureBox_money2
+            // 
+            this.pictureBox_money2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_money2.Image")));
+            this.pictureBox_money2.Location = new System.Drawing.Point(98, 135);
+            this.pictureBox_money2.Name = "pictureBox_money2";
+            this.pictureBox_money2.Size = new System.Drawing.Size(45, 51);
+            this.pictureBox_money2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_money2.TabIndex = 10;
+            this.pictureBox_money2.TabStop = false;
+            // 
+            // label_coins
+            // 
+            this.label_coins.AutoSize = true;
+            this.label_coins.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_coins.Location = new System.Drawing.Point(292, 36);
+            this.label_coins.Name = "label_coins";
+            this.label_coins.Size = new System.Drawing.Size(80, 15);
+            this.label_coins.TabIndex = 11;
+            this.label_coins.Text = "Coins = 0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(384, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.끝내기ToolStripMenuItem});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // 끝내기ToolStripMenuItem
+            // 
+            this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+            this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.끝내기ToolStripMenuItem.Text = "끝내기";
+            this.끝내기ToolStripMenuItem.Click += new System.EventHandler(this.끝내기ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.label_coins);
+            this.Controls.Add(this.pictureBox_money2);
+            this.Controls.Add(this.pictureBox_money3);
+            this.Controls.Add(this.pictureBox_money1);
+            this.Controls.Add(this.label_gameOver);
+            this.Controls.Add(this.pictureBox_redcar);
+            this.Controls.Add(this.pictureBox_Car);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Car Racing";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Car)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_redcar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_money1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_money3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_money2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox_Car;
+        private System.Windows.Forms.PictureBox pictureBox_redcar;
+        private System.Windows.Forms.Label label_gameOver;
+        private System.Windows.Forms.PictureBox pictureBox_money1;
+        private System.Windows.Forms.PictureBox pictureBox_money3;
+        private System.Windows.Forms.PictureBox pictureBox_money2;
+        private System.Windows.Forms.Label label_coins;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
+    }
+}
+
---
+++ CarRacing/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRacing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int carSpeed = 0; //자동차 스피드
+        int collectedcoin = 0;
+
+        void coinsCollection()
+        {
+            if (pictureBox_Car.Bounds.IntersectsWith(pictureBox_money1.Bounds))
+            {
+                collectedcoin++;
+                label_coins.Text = "Coins" + collectedcoin.ToString();
+
+                x = r.Next(0, 200); // 돈을 먹으면 다시 돈이 새로운 랜덤한 지점에서 생기게 한다
+                pictureBox_money1.Location = new Point(x, 0);
+            }
+
+            if (pictureBox_Car.Bounds.IntersectsWith(pictureBox_money2.Bounds))
+            {
+                collectedcoin++;
+                label_coins.Text = "Coins" + collectedcoin.ToString();
+
+                x = r.Next(100, 300);
+                pictureBox_money2.Location = new Point(x, 0);
+            }
+
+            if (pictureBox_Car.Bounds.IntersectsWith(pictureBox_money3.Bounds))
+            {
+                collectedcoin++;
+                label_coins.Text = "Coins" + collectedcoin.ToString();
+
+                x = r.Next(200, 400);
+                pictureBox_money3.Location = new Point(x, 0);
+            }
+        }
+
+        void gameover()
+        {
+            if (pictureBox_Car.Bounds.IntersectsWith(pictureBox_redcar.Bounds))
+            {
+                timer1.Enabled = false;
+                label_gameOver.Visible = true;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveline(carSpeed);
+            redCar(carSpeed);
+            gameover();
+            coins(carSpeed);
+            coinsCollection();
+
+            
+            if (pictureBox_Car.Bounds.IntersectsWith(pictureBox_redcar.Bounds))
+            {
+                timer1.Stop();
+
+                DialogResult result = MessageBox.Show("Game Over! 다시 시작하시겠습니까?", "확인", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    carSpeed = 0;
+                    pictureBox_Car.Location = new Point(38, 293);
+                    pictureBox_redcar.Location = new Point(15, 40);
+                    label_gameOver.Visible = false;
+                    pictureBox_money1.Location = new Point(255, 88);
+                    pictureBox_money2.Location = new Point(98, 135);
+                    pictureBox_money3.Location = new Point(173, 291);
+                    collectedcoin = 0;
+                    label_coins.Text = "Coins" + collectedcoin.ToString(); // 두 개 다 바꿔야 바뀜
+
+                    timer1.Start();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+        }
+
+
+        void coins(int speed)
+        {
+            if(pictureBox_money1.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                pictureBox_money1.Location = new Point(x, 0);
+            }
+            else
+            {
+                pictureBox_money1.Top += speed;
+            }
+
+            if (pictureBox_money2.Top >= 500)
+            {
+                x = r.Next(100, 300);
+                pictureBox_money2.Location = new Point(x, 0);
+            }
+            else
+            {
+                pictureBox_money2.Top += speed;
+            }
+
+            if (pictureBox_money3.Top >= 500)
+            {
+                x = r.Next(200, 400);
+                pictureBox_money3.Location = new Point(x, 0);
+            }
+            else
+            {
+                pictureBox_money3.Top += speed;
+            }
+        }
+
+        Random r = new Random();
+        int x;
+
+        void redCar(int speed)
+        {
+            if(pictureBox_redcar.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                pictureBox_redcar.Location = new Point(x, 0);
+            }
+            else
+            {
+                pictureBox_redcar.Top += speed;
+            }
+
+        }
+
+        void moveline(int speed)
+        {
+            if(pictureBox1.Top >= 500)
+            {
+                pictureBox1.Top = 0;
+            }
+            else
+            {
+                pictureBox1.Top += speed;
+            }
+
+            if (pictureBox2.Top >= 500)
+            {
+                pictureBox2.Top = 0;
+            }
+            else
+            {
+                pictureBox2.Top += speed;
+            }
+
+            if (pictureBox3.Top >= 500)
+            {
+                pictureBox3.Top = 0;
+            }
+            else
+            {
+                pictureBox3.Top += speed;
+            }
+
+            if (pictureBox4.Top >= 500)
+            {
+                pictureBox4.Top = 0;
+            }
+            else
+            {
+                pictureBox4.Top += speed;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if(pictureBox_Car.Left > 0)
+                pictureBox_Car.Left += -10;
+            }
+
+            if(e.KeyCode == Keys.Right)
+            {
+                if(pictureBox_Car.Right < 400 - pictureBox_Car.Width/2)
+                pictureBox_Car.Left += 10;
+            }
+            
+            if(e.KeyCode == Keys.Up)
+            {
+                if(carSpeed < 15)
+                {
+                    carSpeed++;
+                }
+            }
+
+            if(e.KeyCode == Keys.Down)
+            {
+                if(carSpeed > 0)
+                {
+                    carSpeed--;
+                }
+            }
+        }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
